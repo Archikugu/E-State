896f7ec02ec89271a55f450db018ac860496bdd0
Gokhan <engokhangok@gmail.com>
2023-02-19 19:44:59 +0300

    Generic Repository interfaces created for entities

---
+++ DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IRepository<T> where T : class, new()
+    {
+        List<T> List();
+
+        void Add(T item);
+        void Delete(T item);
+        void Update(T item);
+        T GetById(int id);
+        List<T> List(Expression<Func<T, bool>> filter);
+
+    }
+}
