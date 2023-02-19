34d983815309b8c942754927964166760ae92559
Gokhan <engokhangok@gmail.com>
2023-02-19 14:27:55 +0300

    E-State database has been created and packages have been downgraded.

---
+++ DataAccess/Data/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using Entity.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data
+{
+    public class DataContext:IdentityDbContext<UserAdmin>
+    {
+        public DataContext(DbContextOptions options):base(options)
+        {
+            
+        }
+        public DbSet<Advert> Adverts { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<District> Districts { get; set; }
+        public DbSet<GeneralSettings> GeneralSettings { get; set; }
+        public DbSet<Images> Images { get; set; }
+        public DbSet<Neighbourhood> Neighbourhoods { get; set; }
+        public DbSet<Situation> Situations { get; set; }
+        public DbSet<Entity.Entities.Type> Types { get; set; }
+        public DbSet<UserAdmin> UserAdmins { get; set; }
+    }
+}
