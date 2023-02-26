2535438d215557f1ab902c0c692c5e3f3cc3bb29
Gokhan <engokhangok@gmail.com>
2023-02-26 11:28:02 +0300

    Creating admin user with identity

--- EState.UI/Program.cs
+++ EState.UI/Program.cs
@@ -1,14 +1,41 @@
-using DataAccess.Data;
+﻿using DataAccess.Data;
+using Entity.Entities;
+using EState.UI.Areas.Admin.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
 
 builder.Services.AddDbContext<DataContext>(conf => conf.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")).UseLazyLoadingProxies());
 
+builder.Services.AddIdentity<UserAdmin, IdentityRole>().AddEntityFrameworkStores<DataContext>().AddDefaultTokenProviders();
+
+
+builder.Services.Configure<IdentityOptions>(opt =>
+{
+    opt.SignIn.RequireConfirmedPhoneNumber = false;
+    opt.SignIn.RequireConfirmedEmail = false;
+    opt.SignIn.RequireConfirmedAccount = false;
+
+    opt.Password.RequireDigit = false;
+    opt.Password.RequiredLength = 8;
+    opt.Password.RequireLowercase = false;
+    opt.Password.RequireUppercase = false;
+    opt.Password.RequireNonAlphanumeric = false;
+
+    opt.User.AllowedUserNameCharacters = "abcçdefgğhıijklmnoöprsştuüvyzABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ0123456789-._";
+});
+
+
+
 var app = builder.Build();
+
+app.PrepareDatabase().GetAwaiter().GetResult();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
