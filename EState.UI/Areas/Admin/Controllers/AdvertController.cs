5def3fb903959077620ecec298a1b5837a5b0b08
Gokhan <engokhangok@gmail.com>
2023-02-27 12:54:30 +0300

    Added listing of postings of admin panel admin

---
+++ EState.UI/Areas/Admin/Controllers/AdvertController.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EState.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdvertController : Controller
+    {
+        IAdvertService _advertService;
+
+        public AdvertController(IAdvertService advertService)
+        {
+            _advertService = advertService;
+        }
+
+        public IActionResult Index()
+        {
+            string id = HttpContext.Session.GetString("id");
+
+            var list = _advertService.List(x => x.Status == true && x.UserAdminId == id);
+            return View(list);
+        }
+    }
+}
